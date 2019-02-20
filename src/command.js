3de3378ba8071a0838e55b109c4c656df6a4bccb
Mark Horgan <public@markhorgan.com>
2019-02-20 10:59:07 +0000

    Removed most Cocoa Now using example date/time in data drop-down menu Added more date/time formats

--- src/command.js
+++ src/command.js
@@ -5,22 +5,47 @@ const util = require('util')
 const locale = currentLocale()
 
 const dateFormats = {
-  dayMonthYear: 'd/M/YYYY',
-  dayShortMonth: 'd MMM',
-  dayMonth: 'd MMMM'
-};
+  dayMonthYear: {day: 'numeric', month: 'numeric', year: 'numeric'},
+  dayMonthYearPadded: {day: '2-digit', month: '2-digit', year: 'numeric'},
+  dayShortMonthYear: {day: 'numeric', month: 'short', year: 'numeric'},
+  dayMonth: {day: 'numeric', month: 'numeric'},
+  dayMonthPadded: {day: '2-digit', month: '2-digit'},
+  dayShortMonth: {day: 'numeric', month: 'short'},
+  dayLongMonth: {day: 'numeric', month: 'long'},
+  dayLongMonthYear: {day: 'numeric', month: 'long', year: 'numeric'}
+}
 
 const timeFormats = {
-  hour12: 'hh:mm a',
-  hour24: 'HH:mm'
+  hour12: {hour12: true, hour: 'numeric', minute: '2-digit'},
+  hour24: {hour12: false, hour: '2-digit', minute: '2-digit'},
+  hour24Seconds: {hour12: false, hour: '2-digit', minute:'2-digit', second:'2-digit'}
 }
 
 export function onStartup () {
-  DataSupplier.registerDataSupplier('public.text', 'Date: Day Month Year', 'SupplyDateDayMonthYear')
-  DataSupplier.registerDataSupplier('public.text', 'Date: Day Month (Short)', 'SupplyDateDayShortMonth')
-  DataSupplier.registerDataSupplier('public.text', 'Date: Day Month', 'SupplyDateDayMonth')
-  DataSupplier.registerDataSupplier('public.text', 'Time: 12 Hour', 'SupplyTime12Hour')
-  DataSupplier.registerDataSupplier('public.text', 'Time: 24 Hour', 'SupplyTime24Hour')
+  const date = new Date(currentYear(), 1, 5, 15, 30, 5)
+  const names = new Set()
+  registerDataSupplier(true, dateFormats.dayMonthYear, 'SupplyDateDayMonthYear', date, names)
+  registerDataSupplier(true, dateFormats.dayMonthYearPadded, 'SupplyDateDayMonthYearPadded', date, names)
+  registerDataSupplier(true, dateFormats.dayShortMonthYear, 'SupplyDateDayShortMonthYear', date, names)
+  registerDataSupplier(true, dateFormats.dayMonth, 'SupplyDateDayMonth', date, names)
+  registerDataSupplier(true, dateFormats.dayMonthPadded, 'SupplyDateDayMonthPadded', date, names)
+  registerDataSupplier(true, dateFormats.dayShortMonth, 'SupplyDateDayShortMonth', date, names)
+  registerDataSupplier(true, dateFormats.dayLongMonth, 'SupplyDateDayLongMonth', date, names)
+  registerDataSupplier(true, dateFormats.dayLongMonthYear, 'SupplyDateDayLongMonthYear', date, names)
+  registerDataSupplier(false, timeFormats.hour12, 'SupplyTime12Hour', date, names)
+  registerDataSupplier(false, timeFormats.hour24, 'SupplyTime24Hour', date, names)
+  registerDataSupplier(false, timeFormats.hour24Seconds, 'SupplyTime24HourSeconds', date, names)
+}
+
+function registerDataSupplier(isDate, format, action, date, names) {
+  const dateTimeString = isDate ? date.toLocaleDateString(locale, format) : date.toLocaleTimeString(locale, format)
+  const prefix = isDate ? 'Date' : 'Time'
+  const name = `${prefix}: ${dateTimeString}`
+  // In some locales numbers will always be padded so we want to avoid duplicates
+  if (!names.has(name)) {
+    DataSupplier.registerDataSupplier('public.text', name, action)
+    names.add(name)
+  }
 }
 
 export function onShutdown () {
@@ -31,12 +56,32 @@ export function onSupplyDateDayMonthYear(context) {
   supplyDate(dateFormats.dayMonthYear, context)
 }
 
-export function onSupplyDateDayShortMonth(context) {
-  supplyDate(dateFormats.dayShortMonth, context)
+export function onSupplyDateDayMonthYearPadded(context) {
+  supplyDate(dateFormats.dayMonthYearPadded, context)
+}
+
+export function onSupplyDateDayShortMonthYear(context) {
+  supplyDate(dateFormats.dayShortMonthYear, context)
 }
 
 export function onSupplyDateDayMonth(context) {
   supplyDate(dateFormats.dayMonth, context)
+}
+
+export function onSupplyDateDayMonthPadded(context) {
+  supplyDate(dateFormats.dayMonthPadded, context)
+}
+
+export function onSupplyDateDayShortMonth(context) {
+  supplyDate(dateFormats.dayShortMonth, context)
+}
+
+export function onSupplyDateDayLongMonth(context) {
+  supplyDate(dateFormats.dayLongMonth, context)
+}
+
+export function onSupplyDateDayLongMonthYear(context) {
+  supplyDate(dateFormats.dayLongMonthYear, context)
 }
 
 export function onSupplyTime12Hour(context) {
@@ -47,11 +92,15 @@ export function onSupplyTime24Hour(context) {
   supplyTime(timeFormats.hour24, context)
 }
 
+export function onSupplyTime24HourSeconds(context) {
+  supplyTime(timeFormats.hour24Seconds, context)
+}
+
 function supplyDate(format, context) {
   const dataKey = context.data.key
   const items = util.toArray(context.data.items).map(sketch.fromNative)
   items.forEach((item, index) => {
-    const dateString = localizedDateString(format, randomDate())
+    const dateString = randomDate().toLocaleDateString(locale, format)
     DataSupplier.supplyDataAtIndex(dataKey, dateString, index)
   })
 }
@@ -60,55 +109,43 @@ function supplyTime(format, context) {
   const dataKey = context.data.key
   const items = util.toArray(context.data.items).map(sketch.fromNative)
   items.forEach((item, index) => {
-    const timeString = localizedDateString(format, randomTime())
+    const timeString = randomTime().toLocaleTimeString(locale, format)
     DataSupplier.supplyDataAtIndex(dataKey, timeString, index)
   })
 }
 
-function localizedDateString(format, date) {
-  const dateFormatter = NSDateFormatter.new()
-  dateFormatter.locale = locale
-  dateFormatter.dateFormat = NSDateFormatter.dateFormatFromTemplate_options_locale(format, 0, locale);
-  return dateFormatter.stringFromDate(date)
-}
-
 function randomDate() {
-  const dateComponents = NSDateComponents.new()
-  dateComponents.month = randomInteger(1, 12)
-  const range = NSCalendar.currentCalendar().rangeOfUnit_inUnit_forDate(NSDayCalendarUnit, NSMonthCalendarUnit, startOfMonth(dateComponents.month()))
-  dateComponents.day = randomInteger(range.location, range.length)
-  dateComponents.year = currentYear()
-  return NSCalendar.currentCalendar().dateFromComponents(dateComponents)
+  const monthIndex = randomInteger(0, 11);
+  const year = currentYear()
+  const day = randomInteger(1, daysInMonth(monthIndex, year))
+  return new Date(year, monthIndex, day)
 }
 
 function randomTime() {
-  const dateComponents = NSDateComponents.new()
-  dateComponents.hour = randomInteger(0, 23)
-  dateComponents.minute = randomInteger(0, 59)
-  return NSCalendar.currentCalendar().dateFromComponents(dateComponents)
+  const hours = randomInteger(0, 23)
+  const minutes = randomInteger(0, 59)
+  const seconds = randomInteger(0, 59)
+  const today = new Date()
+  return new Date(today.getFullYear(), today.getMonth(), today.getDate(), hours, minutes, seconds)
 }
 
 function randomInteger(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function startOfMonth(month) {
-  const dateComponents = NSDateComponents.new()
-  dateComponents.day = 1
-  dateComponents.month = month
-  dateComponents.year = currentYear()
-  return NSCalendar.currentCalendar().dateFromComponents(dateComponents)
+function daysInMonth (monthIndex, year) {
+  return new Date(year, monthIndex + 1, 0).getDate();
 }
 
 function currentYear() {
-  return NSCalendar.currentCalendar().component_fromDate(NSCalendarUnitYear, NSDate.date())
+  return new Date().getFullYear()
 }
 
 function currentLocale() {
   // NSLocale.currentLocale() only returns the language that is supported by the host application
-  const regionCode = NSLocale.currentLocale().localeIdentifier().split('_')[1]
-  let languageCode = NSLocale.preferredLanguages()[0].split('-')[0]
-  return NSLocale.alloc().initWithLocaleIdentifier(`${languageCode}_${regionCode}`)
+  const countryCode = NSLocale.currentLocale().localeIdentifier().split('_')[1]
+  const languageCode = NSLocale.preferredLanguages()[0].split('-')[0]
+  return `${languageCode}-${countryCode}`
 }
